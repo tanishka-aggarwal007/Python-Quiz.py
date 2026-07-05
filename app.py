510367a9347e866e0840d8b474f9ed4e5c936496
tanishka-aggarwal007 <tanishka.1agarwal@gmail.com>
2026-07-05 22:07:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 import streamlit as st
+if "start_quiz" not in st.session_state:
+    st.session_state.start_quiz = False
+
 
 st.set_page_config(page_title="Python Quiz", page_icon="🧠")
 
 st.title("🧠 Python Quiz Application")
+if not st.session_state.start_quiz:
 
+    st.title("🧠 Python Quiz Application")
+
+    st.write("### Welcome to the Python Quiz! 🎉")
+
+    player_name = st.text_input("👤 Enter your Name")
+
+    if st.button("🚀 Start Quiz"):
+
+        if player_name.strip():
+
+            st.session_state.player_name = player_name
+            st.session_state.start_quiz = True
+            st.rerun()
+
+        else:
+            st.warning("⚠️ Please enter your name first.")
+
+    st.stop()
+
+
+st.success(f"👋 Welcome, {st.session_state.player_name}!")
 questions = [
     {
         "Question": "Who developed Python?",
@@ -62,18 +87,20 @@ for i, question in enumerate(questions):
     if answer.startswith(question["Answer"]):
         score += 1
 
-if st.button("Submit Quiz"):
+st.session_state.player_name
 
-    st.success(f"Your Score: {score}/{len(questions)}")
+st.success(f"🎉 Congratulations, {st.session_state.player_name}!")
 
-    percentage = (score / len(questions)) * 100
+st.write(f"Your Score: {score}/{len(questions)}")
 
-    st.write(f"Percentage: {percentage:.2f}%")
+percentage = (score / len(questions)) * 100
 
-    if percentage >= 75:
+st.write(f"Percentage: {percentage:.2f}%")
+
+if percentage >= 75:
         st.balloons()
         st.success("🎉 Excellent!")
-    elif percentage >= 50:
+elif percentage >= 50:
         st.info("👍 Good Job!")
-    else:
+else:
         st.warning("📚 Keep Practicing!")
